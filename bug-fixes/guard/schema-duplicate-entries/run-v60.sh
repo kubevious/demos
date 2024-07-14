59ad9759534373f867381c0b9525d6aa057a2af2
Ruben Hakopian <ruben.hakopian@gmail.com>
2024-07-14 16:46:22 -0700

    bug fix example.

---
+++ bug-fixes/guard/schema-duplicate-entries/run-v60.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR="$(dirname $MY_PATH)"
+cd ${MY_DIR}
+
+cat deployment.yaml | docker run --rm -i kubevious/cli:1.0.60 guard --stream --k8s-version v1.29 --ignore-unknown --skip-rules container-env-secret-ref container-env-from-secret-ref pod-spec-volume-secret-ref
